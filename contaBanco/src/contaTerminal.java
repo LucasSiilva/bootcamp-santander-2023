a35e4439f06d4103927df828f262f6820bd11a80
Lucas <82125977+LucasSiilva@users.noreply.github.com>
2023-09-13 14:06:25 -0300

    Add files via upload

---
+++ contaBanco/src/contaTerminal.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+import static java.util.Scanner.*;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class contaTerminal {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Por favor, digite seu nome: ");
+        String nome = scanner.next();
+
+        System.out.println("Por favor, digite o número da Agência: ");
+        String agencia = scanner.next();
+
+        System.out.println("Digite o número da sua conta: ");
+        int numero = scanner.nextInt();
+
+        System.out.println("Digite o saldo da sua conta: ");
+        double saldo = scanner.nextDouble();
+
+        System.out.printf("Olá %s, obrigado por criar uma conta em nosso banco, sua agência é %s, conta %d e seu saldo %.2f já esta disponível para saque", nome, agencia, numero, saldo);
+
+        scanner.close();
+
+    }
+}
